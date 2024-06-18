7a4a0e08c04795ff1e4474fee01ccd1dd4b89ff7
Nuradil <hellowolrdnurik@gmail.com>
2024-06-19 04:32:12 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+document.getElementById('openModalBtn').addEventListener('click', function() {
+    document.getElementById('modal').style.display = 'flex';
+});
+
+document.getElementById('closeModalBtn').addEventListener('click', function() {
+    document.getElementById('modal').style.display = 'none';
+});
