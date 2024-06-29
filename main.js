a0671630943de4cf756b85ca44ca953ecc63807f
NurekeMuss <146543627+NurekeMuss@users.noreply.github.com>
2024-06-29 17:11:56 +0600

    Adaptive version

--- main.js
+++ main.js
@@ -68,86 +68,67 @@ let swiper_ads = new Swiper(".card__slider", {
         }
     },
 });
-const track = document.querySelector('.carousel-track');
-const pagination = document.querySelector('.carousel-pagination');
-const items = document.querySelectorAll('.carousel-item');
-const itemsPerPage = 5;
-let currentIndex = 0;
+let swiper_feed = new Swiper(".feed_slider", {
+    slidesPerView: 3,
+    spaceBetween: 25,
+    loop: true,
+    centerSlide: 'true',
+    fade: 'true',
+    grabCursor: 'true',
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        dynamicBullets: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
 
-function createPagination() {
-    const totalPages = Math.ceil(items.length / itemsPerPage);
-    for (let i = 0; i < totalPages; i++) {
-        const button = document.createElement('button');
-        button.addEventListener('click', () => moveToPage(i));
-        if (i === 0) button.classList.add('active');
-        pagination.appendChild(button);
-    }
-}
+    breakpoints: {
+        320: {
+            slidesPerView: 1,
+        },
+        480: {
+            slidesPerView: 2,
+        },
+        768: {
+            slidesPerView: 3,
+        },
+        1200: {
+            slidesPerView: 3,
+        }
+    },
+});
+let swiper_cert = new Swiper(".cert__slider", {
+    slidesPerView: 3,
+    spaceBetween: 25,
+    loop: true,
+    centerSlide: 'true',
+    fade: 'true',
+    grabCursor: 'true',
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        dynamicBullets: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
 
-function updatePagination() {
-    const buttons = pagination.querySelectorAll('button');
-    buttons.forEach(button => button.classList.remove('active'));
-    buttons[Math.floor(currentIndex / itemsPerPage)].classList.add('active');
-}
-
-function moveCarousel(direction) {
-    const totalPages = Math.ceil(items.length / itemsPerPage);
-    currentIndex += direction * itemsPerPage;
-    if (currentIndex < 0) currentIndex = (totalPages - 1) * itemsPerPage;
-    if (currentIndex >= items.length) currentIndex = 0;
-    track.style.transform = `translateX(-${currentIndex * (100 / itemsPerPage)}%)`;
-    updatePagination();
-}
-
-function moveToPage(pageIndex) {
-    currentIndex = pageIndex * itemsPerPage;
-    track.style.transform = `translateX(-${currentIndex * (100 / itemsPerPage)}%)`;
-    updatePagination();
-}
-
-createPagination();
-
-const testimonialTrack = document.querySelector('.testimonial-carousel-track');
-const testimonialPagination = document.querySelector('.testimonial-carousel-pagination');
-const testimonialItems = document.querySelectorAll('.testimonial-carousel-item');
-const testimonialItemsPerPage = 3;
-let testimonialCurrentIndex = 0;
-
-function createTestimonialPagination() {
-    const totalPages = Math.ceil(testimonialItems.length / testimonialItemsPerPage);
-    for (let i = 0; i < totalPages; i++) {
-        const button = document.createElement('button');
-        button.addEventListener('click', () => moveToTestimonialPage(i));
-        if (i === 0) button.classList.add('active');
-        testimonialPagination.appendChild(button);
-    }
-}
-
-function updateTestimonialPagination() {
-    const buttons = testimonialPagination.querySelectorAll('button');
-    buttons.forEach(button => button.classList.remove('active'));
-    buttons[Math.floor(testimonialCurrentIndex / testimonialItemsPerPage)].classList.add('active');
-}
-
-function moveCarousel(direction) {
-    const totalPages = Math.ceil(testimonialItems.length / testimonialItemsPerPage);
-    testimonialCurrentIndex += direction * testimonialItemsPerPage;
-    if (testimonialCurrentIndex < 0) testimonialCurrentIndex = (totalPages - 1) * testimonialItemsPerPage;
-    if (testimonialCurrentIndex >= testimonialItems.length) testimonialCurrentIndex = 0;
-    testimonialTrack.style.transform = `translateX(-${testimonialCurrentIndex * (100 / testimonialItemsPerPage)}%)`;
-    updateTestimonialPagination();
-}
-
-function moveToTestimonialPage(pageIndex) {
-    testimonialCurrentIndex = pageIndex * testimonialItemsPerPage;
-    testimonialTrack.style.transform = `translateX(-${testimonialCurrentIndex * (100 / testimonialItemsPerPage)}%)`;
-    updateTestimonialPagination();
-}
-
-createTestimonialPagination();
-
-
-document.getElementById('burger-menu').addEventListener('click', function() {
-    this.classList.toggle('active');
-    document.getElementById('nav-links').classList.toggle('active');
+    breakpoints: {
+        320: {
+            slidesPerView: 1,
+        },
+        480: {
+            slidesPerView: 2,
+        },
+        768: {
+            slidesPerView: 3,
+        },
+        1200: {
+            slidesPerView: 5,
+        }
+    },
 });
