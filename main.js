aaa751ff5cc230602554eb60056c9c978606649a
NurekeMuss <146543627+NurekeMuss@users.noreply.github.com>
2024-06-30 16:50:26 +0600

    Change p.1

--- main.js
+++ main.js
@@ -10,9 +10,6 @@ let swiper = new Swiper(".slide-content", {
     slidesPerView: 3,
     spaceBetween: 25,
     loop: true,
-    centerSlide: 'true',
-    fade: 'true',
-    grabCursor: 'true',
     pagination: {
         el: ".swiper-pagination",
         clickable: true,
